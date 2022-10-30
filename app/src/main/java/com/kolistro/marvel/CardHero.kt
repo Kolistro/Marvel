b6faa5251818ffa0963c6708485b54fa06e6ce50
Колистратова Юлия <Kolistratova.julija@mail.ru>
2022-10-31 05:01:19 +0600

    lab_2 [+] Добавлена страница с информацией о герое [*] Изменено название функции Screen на MainScreen [+] Добавлена новигация по страницам, в том числе можно вернуться назад [+] Картинки теперь берутся из сети по URL

--- app/src/main/java/com/kolistro/marvel/CardHero.kt
+++ app/src/main/java/com/kolistro/marvel/CardHero.kt
@@ -1,42 +1,58 @@
 package com.kolistro.marvel
 
-import androidx.compose.foundation.Image
-import androidx.compose.foundation.layout.Box
-
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.MutableState
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.semantics.Role.Companion.Image
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.*
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.navigation.NavHostController
+import coil.compose.AsyncImage
+import coil.request.ImageRequest
+import com.kolistro.marvel.navigation.NavRoute
 
 /**
  * Отвечает за внешний вид карточки героя
- * @param item параметр класса ItemHero, отвечает за данные о герое (фото, имя, цвет)
- *
+ * @param hero параметр класса ItemHero, отвечает за данные о герое (фото, имя, цвет)
+ * @param navController компонент навигации
  */
 @Composable
-fun CardHero(item: ItemHero) {
-    Box(
-        contentAlignment = Alignment.BottomCenter,
-    ){
+fun CardHero(navController: NavHostController, hero: MutableState<ItemHero>) {
+    Box(modifier = Modifier,
+        contentAlignment = Alignment.BottomStart,
+    ){/*
         Image(modifier= Modifier
-            .clip(RoundedCornerShape(16.dp)),
-              painter = painterResource(id = item.imageId),
-              contentDescription = "image",
+            .size(100.dp)
+            .clip(RoundedCornerShape(16.dp))
+            .clickable { navController.navigate(NavRoute.HeroScreen.route)},
+              painter = painterResource(id = hero.value.imageId),
+              contentDescription = hero.value.name
+        )*/
+        AsyncImage(
+            modifier= Modifier.fillMaxWidth()
+                .clip(RoundedCornerShape(16.dp))
+                .clickable { navController.navigate(NavRoute.HeroScreen.route)},
+            placeholder = painterResource(hero.value.imageId),
+            model = "https://terrigen-cdn-dev.marvel.com/content/prod/1x/${hero.value.link}",
+            contentDescription = hero.value.description,
+
         )
-
-        Text(text = item.name,
+        Text(text = hero.value.name,
             color = Color.White,
-            fontSize = 30.sp,
-            fontWeight = FontWeight.Bold
-
+            fontSize = 40.sp,
+            fontWeight = FontWeight.Bold,
+            modifier = Modifier.padding(25.dp, 15.dp)
         )
     }
-
 }
--- app/src/main/java/com/kolistro/marvel/ItemHero.kt
+++ app/src/main/java/com/kolistro/marvel/ItemHero.kt
@@ -3,7 +3,9 @@ package com.kolistro.marvel
 import androidx.compose.ui.graphics.Color
 
 data class ItemHero(
+    val link: String,
     val imageId: Int,
     val name: String,
     val color: Color,
+    val description: String
 )
