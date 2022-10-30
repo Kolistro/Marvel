c05a53a0d9478efeb613841fc86d9c6f8228efbe
Колистратова Юлия <Kolistratova.julija@mail.ru>
2022-10-30 18:44:55 +0600

    [*] Исправлено предупреждение от Detekt

--- app/src/main/java/com/kolistro/marvel/CardHero.kt
+++ app/src/main/java/com/kolistro/marvel/CardHero.kt
@@ -1,8 +1,8 @@
 package com.kolistro.marvel
 
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.layout.Box
+
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
@@ -10,10 +10,8 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.modifier.modifierLocalOf
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 
